d942fbf503f60e4d8928e5e357c634bbe602b765
coubs <john.mccoubrie15@gmail.com>
2023-11-12 21:56:49 -0500

    Implemented 2 Unit Tests
    Implemented a units test for a test database with two test Project Tasks.

---
+++ TaskManagementWebApp/TaskManagementWebAppUnitTests/ProjectTasksApiTests.cs
@@ -0,0 +1,62 @@
+using Moq;
+using Microsoft.EntityFrameworkCore;
+using TaskManagementWebApp.Controllers;
+using TaskManagementWebApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaskManagementWebApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+namespace TaskManagementWebAppUnitTests
+{
+    [TestClass]
+    public class ProjectTasksApiTests
+    {
+        [TestMethod]
+        public async Task GetTasks_ReturnsAllTasks()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            // Insert seed data into the database using one instance of the context
+            using (var context = new ApplicationDbContext(options))
+            {
+                context.ProjectTasks.Add(new ProjectTask {
+                    ProjectTaskId = 1,
+                    Title = "Task 1",
+                    Description = "Description for Task 1",
+                    DueDate = DateTime.Now.AddDays(10),
+                    AssignedUserId = "user1",
+                    Status = 0
+                });
+                context.ProjectTasks.Add(new ProjectTask {
+                    ProjectTaskId = 2,
+                    Title = "Task 2",
+                    Description = "Description for Task 2",
+                    DueDate = DateTime.Now.AddDays(20),
+                    AssignedUserId = "user2",
+                    Status = 0
+                });
+                await context.SaveChangesAsync();
+            }
+
+            // Use a clean instance of the context to run the test
+            using (var context = new ApplicationDbContext(options))
+            {
+                var controller = new ProjectTasksApiController(context);
+
+                // Act
+                var result = await controller.GetTasks();
+
+                // Assert
+                Assert.AreEqual(2, result.Value.Count());
+            }
+        }
+    }
+}
