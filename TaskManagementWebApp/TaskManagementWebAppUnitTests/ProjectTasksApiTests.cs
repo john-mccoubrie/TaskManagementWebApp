6a445eacb2bf5b4f48b138c6de108c1e6209bdc0
coubs <john.mccoubrie15@gmail.com>
2023-11-12 22:05:51 -0500

    Fixed package errors
    NuGet Packages were not compatible with each other, changed Microsoft.EntityFrameworkCore.InMemory from 7xx to 6xx.

--- TaskManagementWebApp/TaskManagementWebAppUnitTests/ProjectTasksApiTests.cs
+++ TaskManagementWebApp/TaskManagementWebAppUnitTests/ProjectTasksApiTests.cs
@@ -1,14 +1,7 @@
-using Moq;
 using Microsoft.EntityFrameworkCore;
 using TaskManagementWebApp.Controllers;
 using TaskManagementWebApp.Models;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using TaskManagementWebApp.Data;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
 
 
 namespace TaskManagementWebAppUnitTests
